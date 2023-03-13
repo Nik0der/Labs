bcef907a20d5b960bb55a12ebdcb0621d7fd27a5
Nik0der <124036698+Nik0der@users.noreply.github.com>
2023-03-13 22:44:48 +0300

    Add files via upload

---
+++ Weather week.py
@@ -0,0 +1,10 @@
+import requests
+city = "Kuz’minki, RU"
+appid = "1c120e8db884c1e24eabe2a8020b4cae"
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
+    print("____________________________")
